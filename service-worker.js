540e766d5a8950fdd5d591a649c340fd1babfb36
BackGwa <backgwa@icloud.com>
2023-05-17 18:19:51 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
-self.Minefork = {
+self.Minecraft = {
   version: `Minecraft Classic`,
   cache: true
 };
 self.addEventListener("activate",event => {
   event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
-    if (cache.startsWith("Minefork") && cache !== Minefork.version) return caches.delete(cache);
+    if (cache.startsWith("Minecraft") && cache !== Minecraft.version) return caches.delete(cache);
   }))));
   event.waitUntil(clients.claim());
 });
 self.addEventListener("fetch",event => {
   event.respondWith(caches.match(event.request).then(response => {
     return response || fetch(event.request).then(async response => {
-      if (Minefork.cache) caches.open(Minefork.version).then(cache => cache.put(event.request,response));
+      if (Minecraft.cache) caches.open(Minecraft.version).then(cache => cache.put(event.request,response));
       return response.clone();
     });
   }));
